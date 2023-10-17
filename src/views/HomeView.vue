fb2e93d44714c8f0a9ce20a8418e551ba3ea27ce
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-10-17 14:51:16 +0500

    started app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    Home view
+  </div>
 </template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+  
+</style>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="">
         Register page
     </div>
 </template>

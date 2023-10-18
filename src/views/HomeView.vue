6eff708204ddc20e7eb5b8afec4b1d18559c9203
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-10-18 14:54:57 +0500

    vuex - state manegement

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    Home view
+    Home view {{ counter }}
+    <Button @click="increament">Increase</Button>
+    <Button @click="decreament">Decrease</Button>
   </div>
 </template>
 <script>
 export default {
-  
+    computed: {
+      counter(){
+        return this.$store.state.count
+      }
+    },
+    methods: {
+      increament(){
+        this.$store.commit('increment')
+      },
+      decreament(){
+        this.$store.commit('decrement')
+      }
+    }
 }
 </script>
 <style>
